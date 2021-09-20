a41da1ec3bd00ee2e240d6224ba7691097c6630f
Gunhoo Yoon <gunhoo93@gmail.com>
2021-09-19 22:50:40 -0400

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function (x, y) {
+    if (x < 0 || y < 0 || [x, y].some(val => typeof val !== 'number')) {
+        return "ERROR";
+    }
 
+    const n = Math.min(x, y);
+    const m = Math.max(x, y);
+
+    return (n + m) * (m - n + 1) / 2;
 };
 
 // Do not edit below this line
