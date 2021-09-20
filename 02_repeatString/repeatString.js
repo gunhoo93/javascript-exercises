9456dadf89825b8f29f512d63ad9c719ddc5fdbe
Gunhoo Yoon <gunhoo93@gmail.com>
2021-09-19 22:22:24 -0400

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function (str, n) {
+    if (n < 0) {
+        return 'ERROR';
+    }
+    return new Array(n).fill(str, 0, n).join('');
 };
 
 // Do not edit below this line
