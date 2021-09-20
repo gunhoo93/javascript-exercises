f3142b49ac69574d5311a996007b55301c4cf196
Gunhoo Yoon <gunhoo93@gmail.com>
2021-09-19 22:31:26 -0400

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function(str) {
-
+const reverseString = function (str) {
+    const reversed = [];
+    for (let i = 0; i < str.length; ++i) {
+        reversed[str.length - i] = str[i];
+    }
+    return reversed.join('');
 };
 
 // Do not edit below this line
