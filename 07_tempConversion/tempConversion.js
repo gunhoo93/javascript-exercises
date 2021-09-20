4e530c9313e6f1521ffcfaf9d80630915f4fa2c6
Gunhoo Yoon <gunhoo93@gmail.com>
2021-09-20 19:13:45 -0400

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const ftoc = function() {
-
+const round = function round(n, scale) {
+  const m = 10 ** scale;
+  return Math.round(n * m) / m;
 };
 
-const ctof = function() {
+const ftoc = function (f) {
+  const c = (f - 32) / 1.8;
+  return round(c, 1);
+};
 
+const ctof = function (c) {
+  const f = 1.8 * c + 32;
+  return round(f, 1);
 };
 
 // Do not edit below this line
