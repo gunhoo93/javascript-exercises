9eff94e63706c1d4af486ec9ed222579cbfb4c70
Gunhoo Yoon <gunhoo93@gmail.com>
2021-09-20 19:54:29 -0400

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    n = parseInt(n);
+    if (n < 0) {
+        return "OOPS";
+    }
 
+    let fib = 1;
+    for (let a = 1, b = 1; n > 2; --n) {
+        fib = a + b;
+        a = b;
+        b = fib;
+    }
+
+    return fib;
 };
 
 // Do not edit below this line
