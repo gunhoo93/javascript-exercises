babc865e2881b96b46a9f7ea0c53914a5bc8cdcf
Gunhoo Yoon <gunhoo93@gmail.com>
2021-09-20 19:38:11 -0400

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const sanitizedStr = str.replace(/\W+/g, '').toLowerCase();
+    for (let i = 0, j = sanitizedStr.length - 1; i <= j; ++i, --j) {
+        if (sanitizedStr[i] !== sanitizedStr[j]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
