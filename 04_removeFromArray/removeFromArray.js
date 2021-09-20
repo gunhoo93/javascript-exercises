652dc60545b0a2a7e37a9e443e493cfbebba0da8
Gunhoo Yoon <gunhoo93@gmail.com>
2021-09-19 22:39:21 -0400

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...values) {
+    const result = [];
+    arr.forEach(elem => {
+        if (!values.find(value => elem === value)) {
+            result.push(elem);
+        }
+    });
+    return result;
 };
 
 // Do not edit below this line
