11791978e109812a08ad5e35f2bff53fb048d106
Gunhoo Yoon <gunhoo93@gmail.com>
2021-09-19 23:00:44 -0400

    Complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
+// Leap year is always divisible by 400 or divisible by 4 but not 100.
+const leapYears = function (year) {
+    const divisible = (n, m) => n % m === 0;
 
+    return divisible(year, 400) || (divisible(year, 4) && !divisible(year, 100));
 };
 
 // Do not edit below this line
