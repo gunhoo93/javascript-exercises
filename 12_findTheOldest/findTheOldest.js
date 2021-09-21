53c109f1964c81b74e05c96e17b8a975df2d31fd
Gunhoo Yoon <gunhoo93@gmail.com>
2021-09-20 20:37:28 -0400

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    const currentyYear = (new Date()).getFullYear();
+    const ageOf = ({ yearOfBirth, yearOfDeath = currentyYear }) => yearOfDeath - yearOfBirth;
+    const oldest = (person, other) => ageOf(person) > ageOf(other) ? person : other;
 
+    return people.reduce(oldest);
 };
 
 // Do not edit below this line
